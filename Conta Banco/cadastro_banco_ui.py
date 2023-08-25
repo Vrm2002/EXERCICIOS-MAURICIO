a5c2a60a7bd6eac97cb47ad5814165655e9d8460
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-08-25 16:38:50 -0400

    troquei o Qt

--- Conta Banco/cadastro_banco_ui.py
+++ Conta Banco/cadastro_banco_ui.py
@@ -16,29 +16,28 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QLineEdit,
-    QMainWindow, QPushButton, QSizePolicy, QVBoxLayout,
-    QWidget)
-import sys
+    QMainWindow, QPushButton, QSizePolicy, QTextBrowser,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(997, 664)
+        MainWindow.resize(997, 779)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout_7 = QVBoxLayout(self.centralwidget)
-        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.verticalLayout_8 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
         self.frame_cadastro_banco = QFrame(self.centralwidget)
         self.frame_cadastro_banco.setObjectName(u"frame_cadastro_banco")
         self.frame_cadastro_banco.setFrameShape(QFrame.StyledPanel)
         self.frame_cadastro_banco.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_6 = QVBoxLayout(self.frame_cadastro_banco)
-        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.verticalLayout_7 = QVBoxLayout(self.frame_cadastro_banco)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
         self.lbl_cadastro_banco = QLabel(self.frame_cadastro_banco)
         self.lbl_cadastro_banco.setObjectName(u"lbl_cadastro_banco")
 
-        self.verticalLayout_6.addWidget(self.lbl_cadastro_banco)
+        self.verticalLayout_7.addWidget(self.lbl_cadastro_banco)
 
         self.frame_numero_conta = QFrame(self.frame_cadastro_banco)
         self.frame_numero_conta.setObjectName(u"frame_numero_conta")
@@ -57,7 +56,7 @@ class Ui_MainWindow(object):
         self.verticalLayout.addWidget(self.txt_numero_da_conta)
 
 
-        self.verticalLayout_6.addWidget(self.frame_numero_conta)
+        self.verticalLayout_7.addWidget(self.frame_numero_conta)
 
         self.frame_nome_do_titular = QFrame(self.frame_cadastro_banco)
         self.frame_nome_do_titular.setObjectName(u"frame_nome_do_titular")
@@ -76,31 +75,31 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.addWidget(self.txt_nome_do_titular)
 
 
-        self.verticalLayout_6.addWidget(self.frame_nome_do_titular)
+        self.verticalLayout_7.addWidget(self.frame_nome_do_titular)
 
-        self.frame_valor_de_deposito = QFrame(self.frame_cadastro_banco)
-        self.frame_valor_de_deposito.setObjectName(u"frame_valor_de_deposito")
-        self.frame_valor_de_deposito.setFrameShape(QFrame.StyledPanel)
-        self.frame_valor_de_deposito.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_3 = QVBoxLayout(self.frame_valor_de_deposito)
+        self.frame_deposito_inicial = QFrame(self.frame_cadastro_banco)
+        self.frame_deposito_inicial.setObjectName(u"frame_deposito_inicial")
+        self.frame_deposito_inicial.setFrameShape(QFrame.StyledPanel)
+        self.frame_deposito_inicial.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_3 = QVBoxLayout(self.frame_deposito_inicial)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.lbl_valor_de_deposito = QLabel(self.frame_valor_de_deposito)
-        self.lbl_valor_de_deposito.setObjectName(u"lbl_valor_de_deposito")
+        self.lbl_deposito_inicial = QLabel(self.frame_deposito_inicial)
+        self.lbl_deposito_inicial.setObjectName(u"lbl_deposito_inicial")
 
-        self.verticalLayout_3.addWidget(self.lbl_valor_de_deposito)
+        self.verticalLayout_3.addWidget(self.lbl_deposito_inicial)
 
-        self.txt_valor_de_deposito = QLineEdit(self.frame_valor_de_deposito)
-        self.txt_valor_de_deposito.setObjectName(u"txt_valor_de_deposito")
+        self.txt_deposito_inicial = QLineEdit(self.frame_deposito_inicial)
+        self.txt_deposito_inicial.setObjectName(u"txt_deposito_inicial")
 
-        self.verticalLayout_3.addWidget(self.txt_valor_de_deposito)
+        self.verticalLayout_3.addWidget(self.txt_deposito_inicial)
 
 
-        self.verticalLayout_6.addWidget(self.frame_valor_de_deposito)
+        self.verticalLayout_7.addWidget(self.frame_deposito_inicial)
 
         self.btn_cadastrar = QPushButton(self.frame_cadastro_banco)
         self.btn_cadastrar.setObjectName(u"btn_cadastrar")
 
-        self.verticalLayout_6.addWidget(self.btn_cadastrar)
+        self.verticalLayout_7.addWidget(self.btn_cadastrar)
 
         self.frame_valor_de_saque = QFrame(self.frame_cadastro_banco)
         self.frame_valor_de_saque.setObjectName(u"frame_valor_de_saque")
@@ -108,6 +107,30 @@ class Ui_MainWindow(object):
         self.frame_valor_de_saque.setFrameShadow(QFrame.Raised)
         self.verticalLayout_4 = QVBoxLayout(self.frame_valor_de_saque)
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.frame_deposito = QFrame(self.frame_valor_de_saque)
+        self.frame_deposito.setObjectName(u"frame_deposito")
+        self.frame_deposito.setFrameShape(QFrame.StyledPanel)
+        self.frame_deposito.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_6 = QVBoxLayout(self.frame_deposito)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.lbl_deposito = QLabel(self.frame_deposito)
+        self.lbl_deposito.setObjectName(u"lbl_deposito")
+
+        self.verticalLayout_6.addWidget(self.lbl_deposito)
+
+        self.txt_deposito = QLineEdit(self.frame_deposito)
+        self.txt_deposito.setObjectName(u"txt_deposito")
+
+        self.verticalLayout_6.addWidget(self.txt_deposito)
+
+        self.btn_deposito = QPushButton(self.frame_deposito)
+        self.btn_deposito.setObjectName(u"btn_deposito")
+
+        self.verticalLayout_6.addWidget(self.btn_deposito)
+
+
+        self.verticalLayout_4.addWidget(self.frame_deposito)
+
         self.lbl_valor_de_saque = QLabel(self.frame_valor_de_saque)
         self.lbl_valor_de_saque.setObjectName(u"lbl_valor_de_saque")
 
@@ -119,37 +142,20 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.addWidget(self.txt_valor_de_saque)
 
 
-        self.verticalLayout_6.addWidget(self.frame_valor_de_saque)
-
-        self.btn_deposito = QPushButton(self.frame_cadastro_banco)
-        self.btn_deposito.setObjectName(u"btn_deposito")
-
-        self.verticalLayout_6.addWidget(self.btn_deposito)
+        self.verticalLayout_7.addWidget(self.frame_valor_de_saque)
 
         self.btn_saque = QPushButton(self.frame_cadastro_banco)
         self.btn_saque.setObjectName(u"btn_saque")
 
-        self.verticalLayout_6.addWidget(self.btn_saque)
+        self.verticalLayout_7.addWidget(self.btn_saque)
 
-        self.frame_resultados = QFrame(self.frame_cadastro_banco)
-        self.frame_resultados.setObjectName(u"frame_resultados")
-        self.frame_resultados.setMinimumSize(QSize(150, 150))
-        self.frame_resultados.setFrameShape(QFrame.StyledPanel)
-        self.frame_resultados.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_5 = QVBoxLayout(self.frame_resultados)
-        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
-        self.txt_resultado = QLineEdit(self.frame_resultados)
-        self.txt_resultado.setObjectName(u"txt_resultado")
-        self.txt_resultado.setMinimumSize(QSize(150, 150))
-        self.txt_resultado.setReadOnly(True)
+        self.txtb_resultado = QTextBrowser(self.frame_cadastro_banco)
+        self.txtb_resultado.setObjectName(u"txtb_resultado")
 
-        self.verticalLayout_5.addWidget(self.txt_resultado)
+        self.verticalLayout_7.addWidget(self.txtb_resultado)
 
 
-        self.verticalLayout_6.addWidget(self.frame_resultados)
-
-
-        self.verticalLayout_7.addWidget(self.frame_cadastro_banco)
+        self.verticalLayout_8.addWidget(self.frame_cadastro_banco)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -163,9 +169,14 @@ class Ui_MainWindow(object):
         self.lbl_cadastro_banco.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-size:10pt;\">Cadastro Banco</span></p></body></html>", None))
         self.lbl_numero_da_conta.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:10pt;\">Numero Da Conta:</span></p></body></html>", None))
         self.lbl_nome_do_titular.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:10pt;\">Nome Do Titular:</span></p></body></html>", None))
-        self.lbl_valor_de_deposito.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:10pt;\">Valor De Dep\u00f3sito:</span></p></body></html>", None))
+        self.lbl_deposito_inicial.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:10pt;\">Deposito Inicial:</span></p></body></html>", None))
+        self.txt_deposito_inicial.setPlaceholderText("")
         self.btn_cadastrar.setText(QCoreApplication.translate("MainWindow", u"Cadastrar", None))
-        self.lbl_valor_de_saque.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:10pt;\">Valor de Saque:</span></p></body></html>", None))
+        self.lbl_deposito.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:10pt;\">Deposito:</span></p></body></html>", None))
+        self.txt_deposito.setText("")
+        self.txt_deposito.setPlaceholderText("")
         self.btn_deposito.setText(QCoreApplication.translate("MainWindow", u"Dep\u00f3sito", None))
+        self.lbl_valor_de_saque.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:10pt;\">Valor de Saque:</span></p></body></html>", None))
         self.btn_saque.setText(QCoreApplication.translate("MainWindow", u"Saque", None))
     # retranslateUi
+
