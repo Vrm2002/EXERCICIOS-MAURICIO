6b68b3fba68089763847966249a50f7d733b7621
Gabriel Gravena Barros <129797873+Vrm2002@users.noreply.github.com>
2023-08-28 15:10:10 -0400

    adicionando exercicio 1 completo
    a

--- Conta Banco/cadastro.py
+++ Conta Banco/cadastro.py
@@ -1,7 +1,29 @@
 class Cadastro():
-    def __init__(self, numero_conta, nome, saldo=0):
+    def __init__(self, numero_conta, nome, saldo):
 
 
         self.__numero_conta = numero_conta
         self.nome = nome
         self.__saldo = saldo
+
+
+    def deposito(self,x):
+        self.__saldo = self.__saldo + x
+
+
+    def saque(self,x):
+
+        self.__saldo = self.__saldo - (x+5)
+        
+
+    def saldo_validate(self):
+        if self.__saldo > 0:
+            return True
+        
+
+        else:
+            return False
+        
+
+    def get_saldo (self):
+        return self.__saldo
