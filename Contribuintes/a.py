e9b8922abd57b9c9845cd96deb8ccbeeab99627d
DiegoYokoyama <132004805+oppsggbarros@users.noreply.github.com>
2023-08-30 15:00:05 -0400

    save

--- Contribuintes/a.py
+++ Contribuintes/a.py
@@ -1,30 +1,13 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'Cadastro pessoa juridica.ui'
-##
-## Created by: Qt User Interface Compiler version 6.5.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QLabel,
-    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
-    QTextBrowser, QVBoxLayout, QWidget)
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
 import sys
 
-class Ui_MainWindow(object):
+
+class MainWindow_juridico(QDialog):
     def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        
+        
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout_3 = QVBoxLayout(self.centralwidget)
@@ -333,11 +316,12 @@ class Ui_MainWindow(object):
         self.btn_cadastroJ.setText(QCoreApplication.translate("MainWindow", u"Cadastrar", None))
         self.btn_cadastrar.setText(QCoreApplication.translate("MainWindow", u"Voltar", None))
     # retranslateUi
-
+    
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = QMainWindow()
-    ui = Ui_MainWindow()
+    ui = MainWindow_juridico()
     ui.setupUi(window)
     window.show()
     app.exec()
+    
